24cb4ca6c9a151b1265cdb951a87996e467b9d7f
Quyen Tho <thokimquangquyen@gmail.com>
2020-11-18 18:20:47 +0700

    14. Longest Common Prefix. Not finished yet

---
+++ 14. Longest Common Prefix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _14._Longest_Common_Prefix
+{
+    //https://leetcode.com/problems/longest-common-prefix/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = { "flower", "flow", "flight" };
+
+            Console.WriteLine(LongestCommonPrefix(input));
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string minLengthString = strs.Min();
+
+            string commonPrefix = string.Empty;
+
+            for (int i = 0; i < minLengthString.Length; i++)
+            {
+                commonPrefix = string.Concat(commonPrefix, strs[0][i]);
+
+                for (int j = 1; j < 3; j++)
+                {
+                    if (!strs[j][i].ToString().Equals(commonPrefix[i].ToString()))
+                    {
+                        return commonPrefix.Remove(i);
+                    }
+                }
+            }
+
+            return commonPrefix;
+        }
+    }
+}
