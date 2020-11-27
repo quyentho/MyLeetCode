e0c9b584fe06b5f8b54628393c2ac6ddaa7e2b9a
Quyen Tho <thokimquangquyen@gmail.com>
2020-11-27 23:40:12 +0700

    more problems

---
+++ 1431. Kids With the Greatest Number of Candies/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1431._Kids_With_the_Greatest_Number_of_Candies
+{
+    // https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+    //Input: candies = [4,2,1,1,2], extraCandies = 1
+    //Output: [true,false,false,false,false]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] candies = { 4, 2, 1, 1, 2 };
+            int extraCandies = 1;
+            List<bool> result = (List<bool>)KidsWithCandies(candies, extraCandies);
+            for (int i = 0; i < candies.Length; i++)
+            {
+               Console.WriteLine(result[i]);
+            }
+        }
+
+        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            int greatestCandies = candies.Max();
+            List<bool> result = new List<bool>();
+            for (int i = 0; i < candies.Length; i++)
+            {
+                if (candies[i] + extraCandies >= greatestCandies)
+                {
+                    result.Add(true);
+                }
+                else
+                {
+                    result.Add(false);
+                }    
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ 1480. Running Sum of 1d Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1480._Running_Sum_of_1d_Array
+{
+    // https://leetcode.com/problems/running-sum-of-1d-array/
+
+    /*  Input: nums = [1,2,3,4]
+        Output: [1,3,6,10]
+        Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            int[] result = RunningSum(nums);
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"{result[i] }");
+            }
+        }
+
+        public static int[] RunningSum(int[] nums)
+        {
+            
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+
+            return nums;
+        }
+    }
+}
