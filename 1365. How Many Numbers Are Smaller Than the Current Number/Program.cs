ac956bf42ef90b7234b06e219d93f5f4d1ae66e4
Quyen Tho <thokimquangquyen@gmail.com>
2020-12-10 17:49:25 +0700

    1365. How Many Numbers Are Smaller Than the Current Number

---
+++ 1365. How Many Numbers Are Smaller Than the Current Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _1365._How_Many_Numbers_Are_Smaller_Than_the_Current_Number
+{
+    // https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 6, 5, 4, 8 };
+            int[] result = SmallerNumbersThanCurrent(nums);
+            result.ToList().ForEach(num => Console.Write($"{num} "));
+        }
+
+        public static int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = 0;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (j != i && nums[j] < nums[i])
+                    {
+                        result[i]++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
+
--- 53. Maximum Subarray/Program.cs
+++ 53. Maximum Subarray/Program.cs
@@ -8,7 +8,7 @@ namespace _53._Maximum_Subarray
         static void Main(string[] args)
         {
             int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-            Console.WriteLine(MaxSubArray(nums));
+            Console.WriteLine(MaxSubArrayDAC(nums,0,nums.Length));
         }
 
         public static int MaxSubArray(int[] nums)
@@ -36,14 +36,25 @@ namespace _53._Maximum_Subarray
             return max;
         }
 
-        //public static int MaxSubArrayDAC(int[] nums   )
-        //{
-        //    if (nums.Length == 1)
-        //    {
-        //        return nums[0];
-        //    }
+        public static int MaxSubArrayDAC(int[] nums, int left, int right)
+        {
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
 
+            int mid = (left + right) / 2;
+
+            int max = nums[mid];
+
+            int sum = MaxSubArrayDAC(nums, left, mid) + MaxSubArrayDAC(nums, mid + 1, right);
             
-        //}
+            if (sum > max)
+            {
+                max = sum;
+            }
+
+            return max;
+        }
     }
 }
